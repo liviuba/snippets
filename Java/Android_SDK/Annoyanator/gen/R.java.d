91a6398a815fefb5cc22e2f2a35da8d8fe661034
liviuba <anita.liviu@gmail.com>
2015-04-18 23:08:04 +0300

    Cleanup

--- Java/Android_SDK/Annoyanator/gen/R.java.d
+++
@@ -1,8 +0,0 @@
-/home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/gen/liviu/examples/annoyanator/R.java \
- : /home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/res/drawable-hdpi/ic_launcher.png \
-/home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/res/drawable-xhdpi/ic_launcher.png \
-/home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/res/values/strings.xml \
-/home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/res/drawable-mdpi/ic_launcher.png \
-/home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/res/drawable-ldpi/ic_launcher.png \
-/home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/res/layout/main.xml \
-/home/imv/Documents/Code/Lang/snippets/Java/Android_SDK/Annoyanator/bin/AndroidManifest.xml \
